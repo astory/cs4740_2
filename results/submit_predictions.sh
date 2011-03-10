f3cfa7d812acb1958999180adce1e5da7c49bdc3
Thomas Levine <thomas.levine@gmail.com>
2011-03-10 04:48:37 -0500

    automatically submit pedictions

---
+++ results/submit_predictions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+netid=tkl22
+predictions_file=baseline.pos
+istest=0 #0 or 1
+isbaseline=0 #0 or 1
+degree=2 #n-gram size: 2, 3 or 4 (4 or more)
+casefold=0 #0 or 1
+unkhandle=0 #0 or 1
+transsmoothing=None
+emitsmoothing=None
+#'None' 'AddOne'  'GoodTuring' 'DI'  'Backoff' 'Other'
+
+curl -F predictions=@$predictions_file  --form-string netid=$netid --form-string istest=$istest --form-string isbaseline=$isbaseline --form-string degree=$degree --form-string casefold=$casefold --form-string unkhandle=$unkhandle --form-string transsmoothing=$transsmoothing --form-string emitsmoothing=$emitsmoothing http://www.cs.cornell.edu/w8/~luwang/cs4740/getFile
