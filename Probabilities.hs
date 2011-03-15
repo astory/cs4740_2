0b4d9649a9e4f4127736ff94de7a6955afa76bac
Alec Story <avs38@cornell.edu>
2011-03-15 15:40:51 -0400

    cleanup

--- Probabilities.hs
+++ Probabilities.hs
@@ -57,78 +57,9 @@ baseline = do
         hClose handle
         )
 
---Log probabilities
-toLog p = (log . fromIntegral) p
-fromLog p = exp p
-probDiv num denom = toLog  num - toLog denom
-
-probSum :: [LogProb] -> LogProb
-probSum ins = fromLog $ L.foldr sumExp 1 ins
-     where sumExp x y = sum[x,exp y]
-
---Lexical generation probabilities (emission probabilities) for a particular word|tag
---Everything else
-lexical :: CountMap -> String -> String -> LogProb
-lexical word'tag word tag
-       --If the tag occurs in the training corpus
-      | M.notMember tag word'tag = 0
-       --If the word occurs with that tag in the training corpus
-      | M.notMember word (word'tag M.! tag) = 0
-      -- Otherwise, the probability of the word given the tag
-      | otherwise = (word'tag M.! tag M.! word) `probDiv` (M.fold sum2 0 (word'tag M.! tag)) 
-        where sum2 x y = sum [x,y]
-
-trellisTags :: Trellis -> [Tag]
-trellisTags trellis=(S.elems . M.keysSet) $ last trellis
-
---Ignore ngram for now
-transition :: [Tag] -> LogProb
-transition ngram = -1.2
-
-qNext :: CountMap -> Word -> Trellis -> Tag -> Tag -> LogProb
-qNext word'tag word trellis tagPrev tagNext =
-      (last trellis) M.! tagPrev --Previous state
-    + (transition ngram) --Transition
-    + (lexical word'tag word tagNext) --Lexical
-      where ngram = [tagNext]
-            --Unigram for now, adjust ngram to allow for higher grams
-
---Bigram
-viterbi :: CountMap -> [Word] -> Trellis -> Trellis
-viterbi word'tag wordsPrev trellisPrev
-     | wordsPrev == []     = trellisPrev
-     | trellisPrev == gram = viterbi word'tag wordsPrev trellisStart
-     | otherwise           = viterbi word'tag wordsNext trellisPrev
-     -- ++  M.fold (qNext word'tag word trellisPrev) 1 tags
-       where wordsNext = init wordsPrev
-             word = head wordsPrev
-             gram = []
-             trellisStart= [M.fromList [( "NN", -1.5 ), ("NNP", -2) , ("<s>", -0.1) ]]
-             tags=trellisTags trellisPrev
-
-
---M.map (lexical "elephant" word'tag) ((S.elems . M.keysSet) word'tag)
---(S.elems . M.keysSet) word'tag
---let word'tag = (val'key . sentences) [("<s>","<s>"),("NN","director"),("NN","elephant")]
-
-main' = do
-    training <- getContents
-    withFile "pos_corpora/test-obs.pos" ReadMode (\handle -> do 
-        test <- hGetContents handle
-        let tagged_words = posTag training
-            sents = sentences tagged_words
-            sents' = map (map swap) sents
-            word'tag = val'key sents
-            tag'word = val'key sents'
-
-            test_words = lines test
-        putStrLn $ unlines . map show . zip test_words . map (pick_most_frequent tag'word) $ test_words
-        hClose handle
-        )
-
 main = do
     training <- getContents
-    withFile "pos_corpora/test-obs.pos" ReadMode (\handle -> do 
+    withFile "pos_corpora/test-obs_short.pos" ReadMode (\handle -> do 
         test <- hGetContents handle
         let tagged_words = posTag training
             sents = sentences tagged_words
@@ -137,19 +68,6 @@ main = do
             tag'word = val'key sents'
 
             test_words = lines test
-        --Print the predictions
-        --putStrLn $ unlines . (map show) . zip test_words . map (pick_most_frequent tag'word) $ test_words
-
-        --Print just the tags that were guessed
-        --print $ map snd $ zip test_words . map (pick_most_frequent tag'word) $ test_words
-            l = case M.lookup "'s" tag'word of
-                Nothing -> M.empty
-                Just map -> map
-        
         putStrLn $ unlines . map show . zip test_words . map (pick_most_frequent tag'word) $ test_words
-        
         hClose handle
         )
-
-
-
