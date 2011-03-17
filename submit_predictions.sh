ee2c571269f7242810e57ba04ee58473e70211eb
Thomas Levine <thomas.levine@gmail.com>
2011-03-17 18:21:21 -0400

    submit predictions more nicely

--- submit_predictions.sh
+++ submit_predictions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 netid=$1
 predictions_file=$2
-curl -F predictions=@$predictions_file  --form-string "netid=$netid istest='1' isbaseline=1 degree=2 casefold=0 unkhandle=0 transsmoothing=None emitsmoothing=None" http://www.cs.cornell.edu/w8/~luwang/cs4740/getFile 
+curl -F predictions=@$predictions_file  --form-string netid=$netid \
+--form-string istest=1 --form-string isbaseline=1 --form-string degree=2 \
+--form-string casefold=0 --form-string unkhandle=0 \
+--form-string transsmoothing=None --form-string emitsmoothing=None \
+http://www.cs.cornell.edu/w8/~luwang/cs4740/getFile 
