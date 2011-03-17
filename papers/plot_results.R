a37a66628147b42426c869c89ececa47b4c9c516
Thomas Levine <thomas.levine@gmail.com>
2011-03-17 10:00:42 -0400

    srcipt to plot results

--- papers/plot_results.R
+++ papers/plot_results.R
@@ -1,3 +1,29 @@
 source('experiment_design.R')
-design=treatments
-results=read.csv('results.csv')
+design=treatments[-5] #Remove the score=?
+scores=read.csv('results.csv')
+results=merge(design,scores,by='file')
+
+startplot=function(){
+	plot(score~n,results,type='n',axes=F,
+		xlab='N-gram length',ylab='Score',
+		main='Performance of the part-of-speech tagger'
+	)
+	axis(1,at=min(results$n):max(results$n))
+	axis(2)
+}
+
+startplot()
+points(score~n,subset(results,smooth.gram=='None'),type='l')
+points(score~n,subset(results,smooth.gram=='AddOne'),type='l')
+points(score~n,subset(results,smooth.gram=='GoodTuring'),type='l')
+
+startplot()
+points(score~n,subset(results,smooth.lex=='None'),type='l')
+points(score~n,subset(results,smooth.lex=='AddOne'),type='l')
+points(score~n,subset(results,smooth.lex=='GoodTuring'),type='l')
+
+startplot()
+points(score~n,subset(results,unk=='None'),type='l')
+points(score~n,subset(results,unk=='FirstOccurrance'),type='l')
+points(score~n,subset(results,unk=='Count=1'),type='l')
+
