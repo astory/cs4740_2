290f179971ca7ec42bb38fcbf19dd8737d3b4bda
Thomas Levine <thomas.levine@gmail.com>
2011-03-17 09:48:32 -0400

    don't remove .pos from results

--- tabulate_results.sh
+++ tabulate_results.sh
@@ -2,4 +2,4 @@
 files="$1" #Input
 
 echo id,score 
-sed -n -e 's/\.pos//' -e 's/^score//p' -e s/^filename//p $files|tac|perl -pi -e 'tr/[\012\015]/e/d'|sed 's/%/\n/g'|sed -e s/^\ *//g|sed s/\ \ */,/ 
+sed -n -e 's/^score//p' -e s/^filename//p $files|tac|perl -pi -e 'tr/[\012\015]/e/d'|sed 's/%/\n/g'|sed -e s/^\ *//g|sed s/\ \ */,/ 
